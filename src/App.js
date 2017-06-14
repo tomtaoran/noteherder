8b1edd30d21826d576907b89b612cd69c4c789bd
Tom <tomtaoran@aliyun.com>
2017-06-14 16:17:24 -0400

    Add Sidebar NoteList NoteForm Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Sidebar from './Sidebar'
+import NoteList from './NoteList'
+import NoteForm from './NoteForm'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        NoteHerder
+        <Sidebar />
+        <NoteList />
+        <NoteForm />
       </div>
     );
   }
---
+++ src/Sidebar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Sidebar= ()=>{
+ return <p>Sidebar!</p>
+}
+
+
+export default Sidebar
