e8d94bd0a1475842312d46711bc4e23ae08f4421
Tom <tomtaoran@aliyun.com>
2017-06-14 16:30:12 -0400

    Load images and script

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Sidebar from './Sidebar'
+import NoteList from './NoteList'
+import NoteForm from './NoteForm'
+
+const Main = () =>{
+    return (
+        <div className="Main">
+        <Sidebar />
+        <NoteList />
+        <NoteForm />
+        </div>
+    )
+}
+
+export default Main
